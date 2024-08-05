72f10d6bdd3fe2f45435758c327f520a13fa03f5
Jagadish Gowda P <jagadishp.gowda@informa.com>
2024-08-05 19:20:45 +0530

    assignment 1

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
 // const heading = React.createElement('h1',{id:"heading"}, "Hello World from React");
 // const root = ReactDOM.createRoot(document.getElementById("head"));
 // root.render(heading)
@@ -11,7 +14,7 @@
 
 const heading = React.createElement("div", { id: "parent" },
     React.createElement("div", { id: "child" },
-        [React.createElement("h1", {}, "I'm h1 tag"),
+        [React.createElement("h1", {}, "I'm inside h1 tag"),
         React.createElement("h2", {}, "I'm h1 tag")
         ]
     ))
